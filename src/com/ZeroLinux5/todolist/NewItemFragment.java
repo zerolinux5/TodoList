147e3a4db68373c7fd3c911a8e9aaf6fc3074b04
Jesus Magana <maganazero5@gmail.com>
2014-09-09 18:20:59 -0700

    Added fragment class and xml

---
+++ src/com/ZeroLinux5/todolist/NewItemFragment.java
@@ -0,0 +1,16 @@
+package com.ZeroLinux5.todolist;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewItemFragment extends Fragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.new_item_fragment, container, false);
+	}
+
+}
